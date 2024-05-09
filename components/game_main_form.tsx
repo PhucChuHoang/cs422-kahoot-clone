f31036d82f460f44d06c02a2a8c64280b1bdc086
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2024-05-09 10:35:30 +0700

    - add answer button

--- components/game_main_form.tsx
+++ components/game_main_form.tsx
@@ -1,16 +1,32 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Card, CardContent, CardHeader } from './ui/card';
 import { Progress } from './ui/progress';
+import { Button } from './ui/button';
+
+const total = 10;
 
 export const GameMainForm = () => {
-  return (
-    <div className="w-full space-y-10">
-      <Progress value={50} />
+  const [num, setNum] = useState(0);
+  const [answered, setAnswered] = useState(false);
 
-      <div className="h-full w-full justify-between">
+  async function onSubmit() {
+    if (answered) {
+      return;
+    }
+    setNum((num) => num + 1);
+    if (num === total) {
+      setAnswered(true);
+    }
+  }
+
+  return (
+    <div className="h-full w-full space-y-10">
+      <Progress value={(num / total) * 100} />
+
+      <div className="w-full justify-center">
         <Card className="bg-yellow-400">
           <CardHeader className="items-center text-white">
             <h1 className="text-xl font-bold">Question</h1>
@@ -29,6 +45,38 @@ export const GameMainForm = () => {
             </div>
           </CardContent>
         </Card>
+      </div>
+
+      <div className="mx-20 flex justify-between gap-10">
+        <Button
+          onClick={onSubmit}
+          className="h-20 flex-grow rounded bg-red-400 px-4 py-2 text-xl font-bold text-white hover:bg-red-500"
+        >
+          Answer1
+        </Button>
+
+        <Button
+          onClick={onSubmit}
+          className="h-20 flex-grow rounded bg-blue-400 px-4 py-2 text-xl font-bold text-white hover:bg-blue-500"
+        >
+          Answer2
+        </Button>
+      </div>
+
+      <div className="mx-20 flex justify-between gap-10">
+        <Button
+          onClick={onSubmit}
+          className="h-20 flex-grow rounded bg-yellow-400 px-4 py-2 text-xl font-bold text-white hover:bg-yellow-500"
+        >
+          Answer3
+        </Button>
+
+        <Button
+          onClick={onSubmit}
+          className="h-20 flex-grow rounded bg-green-400 px-4 py-2 text-xl font-bold text-white hover:bg-green-500"
+        >
+          Answer4
+        </Button>
       </div>
     </div>
   );
